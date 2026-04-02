dfcc7bf7078a1d2467bec87c5f55004feb219fb4
sergio <ssantosr6@miumg.edu.gt>
2026-04-02 00:37:23 -0600

    Se realizan modificaciones al Visitor ya que se estaba realizando una acción no valida provocada por no modificar la palabra entonces por sino en la condicional

--- examples/programa1.ml
+++ examples/programa1.ml
@@ -8,12 +8,12 @@ program {
   y = 20;
   z = x + y * 2;  // 10 + 20*2 = 50
 
-  if (x > 10 || y <20) {
+  si (x > 10 || y < 20) {
     w = x / 2;    // división entera -> 25
-  } else {
+  } sino {
     w = y - 5;
   }
 
-  print(z);       // imprime 50
-  print(w);       // imprime 25
+  imprime(z);       // imprime 50
+  imprime(w);       // imprime 25
 }
